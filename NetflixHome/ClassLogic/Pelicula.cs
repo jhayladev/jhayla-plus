1a4bf2659dc6dcfc2243c03305883bdc934a4fd4
Nandaapx <addununeznunezavila@gmail.com>
2026-08-18 08:43:42 -0500

    estructura base slnNetflix

---
+++ NetflixHome/ClassLogic/Pelicula.cs
@@ -0,0 +1,9 @@
+﻿namespace NetflixHome.ClassLogic
+{
+    public class Pelicula
+    {
+        public string Titulo { get; set; }
+        public string ImagenUrl { get; set; }
+        public int Year { get; set; }
+    }
+}
