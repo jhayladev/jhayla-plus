901d7a1682667324cb6448954bd167f7f446698b
Nandaapx <addununeznunezavila@gmail.com>
2026-09-02 09:15:49 -0500

    inicio finalizado, y estable

--- NetflixHome/obj/Debug/net10.0/NetflixHome.AssemblyInfo.cs
+++ NetflixHome/obj/Debug/net10.0/NetflixHome.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetflixHome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcbbadea3b35be97470785d34575d43b603856fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6892e353b7467e319079125a709be2ddcd43800e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetflixHome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetflixHome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
